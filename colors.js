fa4d5da61c3355d651b24b29a73d226af7b4e8ab
sonwh123 <80819521+sonwh123@users.noreply.github.com>
2023-02-20 23:53:37 +0900

    WEB2 JavaScript -34. 파일로 쪼개서 정리 정돈하기

---
+++ colors.js
@@ -0,0 +1,36 @@
+﻿ var Body = {
+   setBackgroundColor : function(color) {
+     document.querySelector('body').style.backgroundColor = color;
+   },
+   setColor : function(color) {
+     document.querySelector('body').style.color = color;
+   }
+ }
+ var Links = {
+   setColor : function(color) {
+     var lis = document.getElementsByTagName('a');
+     for(var i=0; i < lis.length; i++){
+       lis[i].style.color = color;
+     }
+   }
+ }
+ function changeDayNight(c1, c2, self) {
+   Body.setBackgroundColor(c1);
+   Body.setColor(c2);
+   Links.setColor(c2);
+   document.querySelector('h1').style.borderBottomColor = c2;
+   document.getElementById('grid').querySelector('ol').style.borderRightColor = c2;
+   if(c1 === 'black'){
+     self.value = 'day';
+   } else {
+     self.value = 'night';
+   }
+ }
+
+ function nightDayHandler(self) {
+   if(self.value === 'night'){
+     changeDayNight('black', 'white', self);
+   } else {
+     changeDayNight('white', 'black', self);
+   }
+ }
---
+++ menu.js
@@ -0,0 +1,8 @@
+﻿ function showAll(arr){
+   for(var i = 0; i < arr.length; i++) {
+     document.write('<li><a href = "' + (i+1) + '.html">' + arr[i] + '</a></li>');
+   }
+ }
+
+ var menu = ["손영훈", "김준수", "유원준", "김재민"];
+ showAll(menu);
